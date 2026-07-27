1a97cae97f116e49f07d4193973b38b64890975b
marksokolov1 <92471950+marksokolov1@users.noreply.github.com>
2026-07-27 17:00:19 -0400

    1

--- tests/Feature/PublicPagesTest.php
+++ tests/Feature/PublicPagesTest.php
@@ -153,10 +153,8 @@ test('how it works uses clean product screenshots for every workflow step', func
         ->assertSee('images/tutorial/coach/02-exercise-library.png', escape: false)
         ->assertSee('images/tutorial/coach/03-invite-client.png', escape: false)
         ->assertSee('images/tutorial/coach/04-create-program.png', escape: false)
-        ->assertSee('images/tutorial/coach/05-set-nutrition-targets.png', escape: false)
-        ->assertSee('images/tutorial/coach/07-workout-status.png', escape: false)
-        ->assertSee('images/tutorial/coach/08-client-parameters.png', escape: false)
         ->assertSee('images/tutorial/coach/09-nutrition-review.png', escape: false)
+        ->assertDontSee('workflow-app-collage', escape: false)
         ->assertSee('Invited clients activate their FitFreak Pro experience for $12.99 per month through Stripe.');
 });
 
